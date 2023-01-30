63b1d97d1b591657b8b161f6e63916020eaa7254
hayarovy <edurd@rambler.ru>
2023-01-30 14:32:46 +0500

    add database

--- app/src/main/java/com/edurda77/testsport/presentation/MainActivity.kt
+++ app/src/main/java/com/edurda77/testsport/presentation/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.edurda77.testsport
+package com.edurda77.testsport.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.edurda77.testsport.databinding.ActivityMainBinding
 import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/edurda77/testsport/utils/Consts.kt
@@ -0,0 +1,7 @@
+package com.edurda77.testsport.utils
+
+const val NOTE_TABLE = "notes"
+const val NOTE_ID = "id"
+const val NOTE_TITLE = "title"
+const val NOTE_CONTENT = "content"
+const val DATE_RECORD = "date_record"
