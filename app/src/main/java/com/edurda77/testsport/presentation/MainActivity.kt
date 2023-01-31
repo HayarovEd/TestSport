dce21c20334ccda64078007291a70de746dfc5a3
hayarovy <edurd@rambler.ru>
2023-01-31 11:09:17 +0500

    release webView

--- app/src/main/java/com/edurda77/testsport/presentation/MainActivity.kt
+++ app/src/main/java/com/edurda77/testsport/presentation/MainActivity.kt
@@ -4,38 +4,35 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.CookieManager
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.edurda77.testsport.R
 import com.edurda77.testsport.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-@SuppressLint("SetJavaScriptEnabled")
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        val webView = binding.webView
-        webView.webViewClient = WebViewClient()
-        val webSettings = webView.settings
-        //webSettings.javaScriptEnabled = true
-        webView.loadUrl("https://google.com/")
-        //webView.loadUrl("https://ya.ru/")
-        //initWebView(savedInstanceState, url="https://ya.ru/")
+        initWebView(savedInstanceState, url="https://google.com/")
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun initWebView(savedInstanceState: Bundle?, url:String) {
-        //val webView = binding.webView
-        //webView.webViewClient = WebViewClient()
-        //val webSettings = webView.settings
-        //webSettings.javaScriptEnabled = true
-        /*if (savedInstanceState != null)
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        if (savedInstanceState != null) {
             webView.restoreState(savedInstanceState)
-        else*/
-          //  webView.loadUrl("https://ya.ru/")
-        /*webView.settings.domStorageEnabled = true
+        }
+        else {
+            webView.loadUrl("https://ya.ru/")
+        }
+        webView.settings.domStorageEnabled = true
         webView.settings.javaScriptCanOpenWindowsAutomatically = true
         val cookieManager = CookieManager.getInstance()
         cookieManager.setAcceptCookie(true)
@@ -48,7 +45,15 @@ class MainActivity : AppCompatActivity() {
         webSettings.allowFileAccess = true
         webSettings.allowContentAccess = true
         webSettings.loadWithOverviewMode = true
-        webSettings.useWideViewPort = true*/
+        webSettings.useWideViewPort = true
 
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
