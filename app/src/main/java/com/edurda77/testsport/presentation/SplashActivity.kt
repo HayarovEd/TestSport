d160f0482732562e1b8d3e3271d047d755974b92
hayarovy <edurd@rambler.ru>
2023-02-02 12:25:14 +0500

    fix problem loading

---
+++ app/src/main/java/com/edurda77/testsport/presentation/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.edurda77.testsport.presentation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.activity.viewModels
+import com.edurda77.testsport.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    private val viewModel by viewModels<SplashActivityViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+        viewModel.getData()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
+
+    }
+}
