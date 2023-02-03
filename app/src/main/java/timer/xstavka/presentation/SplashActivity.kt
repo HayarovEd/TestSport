1d5fd36c86f67175fe64975dfc134a519220b52c
hayarovy <edurd@rambler.ru>
2023-02-03 07:41:01 +0500

    icon

--- app/src/main/java/timer/xstavka/presentation/SplashActivity.kt
+++ app/src/main/java/timer/xstavka/presentation/SplashActivity.kt
@@ -23,7 +23,7 @@ class SplashActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }, 2000)
+        }, 3000)
 
     }
 }
